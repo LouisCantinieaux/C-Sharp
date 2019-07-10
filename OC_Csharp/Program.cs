86c1ebeec525e95bc950663696718264931d3372
Louis <cantinieauxlouis@gmail.com>
2019-07-10 14:27:13 +0200

    Few basic project in C-sharp

---
+++ OC_Csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OC_Csharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] jours = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+
+            foreach (string jour in jours)
+            {
+                if (jour == "Mercredi")
+                {
+                    jours.Remove(jour);
+                }
+            }
+        }
+    }
+}
